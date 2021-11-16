40cc78bb057ce9a1e47c6f193036d75bb001bd0d
robertonat <robertonat.v@gmail.com>
2021-11-16 16:38:16 -0500

    Decimal places
    This ensures that the account balance goes to two decimal places only. As a bank we are dealing with money and can go only as small as 1 cent as far as users are concerned. Account balance should reflect that now

--- src/components/AccountBalance.js
+++ src/components/AccountBalance.js
@@ -4,10 +4,11 @@ class AccountBalance extends Component {
   render() {
     return (
         <div>
-          Balance: {this.props.accountBalance}
+          Balance: {Math.round(this.props.accountBalance*100)/100}
+
         </div>
     );
   }
 }
-
+/////{Math.round((this.props.accountBalance*100)/100).toFixed(2)}
 export default AccountBalance;
