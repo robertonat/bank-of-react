58cba346274efd40605bbf64af3320d7dde2bcff
robertonat <robertonat.v@gmail.com>
2021-11-16 16:23:09 -0500

    structuring for adding debits
    Added more things to the add debit function for it to fully work. Added a variable to represent the date the debit was added and added the calculation to take todays date and use it to format the date variable. Created a new debit object that in turn is being added to the array of the debits. Also added quick calculations to add to the sum of debits and subtract from the account balance.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import Home from './components/Home';
 import UserProfile from './components/UserProfile';
 import LogIn from './components/Login'
 import Debits from './components/Debits'
 import Credits from './components/Credits'
 import axios from "axios";
+import './App.css'
 
 
 class App extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       accountBalance: 0,
-      debit: [],
-      credit: [],
+      debits: [],
+      credits: [],
       currentUser: {
         userName: 'Homer_Simpson',
         memberSince: '07/23/96',
-      },
+      }
     }
 
   }
@@ -40,7 +41,6 @@ class App extends Component {
     credits.forEach((credit) => {
       creditSum += credit.amount
     })
-
     let accountBalance = creditSum - debitSum;
     this.setState({debits, credits, accountBalance});
   }
@@ -52,9 +52,26 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
+
+  addDebit = (e) => {
+    //send to debits view via props
+    //updates state based off user input
+    e.preventDefault();
+    const today = new Date()
+    const description  = e.target[0].value;
+    const amount  = Number(e.target[1].value);
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    //const date = String(2021)+ "-"+ String(11) + "-" + String(16)
+    const newDebit = {id: this.state.debits.length, amount: amount, description: description, date:date}
+    this.setState({accountBalance: this.state.accountBalance - amount, debitSum: this.state.debitSum+amount, debits: [newDebit, ...this.state.debits]})
+
+  }
+
+
+
   render() {
 
-
+    const {debits} = this.state;
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
 
     const UserProfileComponent = () => (
@@ -63,22 +80,19 @@ class App extends Component {
 
     const LogInComponent = () => (
       <LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />
-    )
+    );
 
-    const DebitComponent = () => (
-      <Debits debits = {this.state.debits}/>
-    )
+    const DebitsComponent = () => (<Debits addDebit={this.addDebit} debits={debits} />);
 
-    const CreditComponent = () => (
-      <Credits credits = {this.state.credits}/>
-    )
+    const CreditComponent = () => (<Credits credits = {this.state.credits}/>);
+
     return (
         <Router>
           <div>
             <Route exact path="/" render={HomeComponent}/>
             <Route exact path="/userProfile" render={UserProfileComponent}/>
             <Route exact path="/login" render={LogInComponent}/>
-            <Route exact path="/debits" render={DebitComponent}/>
+            <Route exact path="/debits" render={DebitsComponent}/>
             <Route exact path="/credits" render={CreditComponent}/>
           </div>
         </Router>
