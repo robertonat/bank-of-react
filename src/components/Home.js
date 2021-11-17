39018cb4b56b65339ee5eecef11a53a5b0c6ff38
robertonat <robertonat.v@gmail.com>
2021-11-17 03:32:22 -0500

    title update
    The title now updates if the user is looking at the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,15 +2,16 @@ import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
 import icon from '../FirstBankofSpringfield.png'
+
 class Home extends Component {
   render() {
+    document.title = "Home"
     return (
         <div>
           <img src={icon} alt="bank"/>
           <h1>Bank of React: Springfield branch</h1>
 
           <Link to="/userProfile">User Profile</Link>
-
           <AccountBalance accountBalance={this.props.accountBalance}/>
           <Link to="/login">log in</Link>
           <br></br>
